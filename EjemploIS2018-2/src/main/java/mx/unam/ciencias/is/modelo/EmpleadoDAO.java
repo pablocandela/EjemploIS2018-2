181bba63154e40beeeef88a3bab739decef9e6c7
jonathan <jobaji09@gmail.com>
2018-03-01 01:06:33 -0600

    Se modifica la creacion de la coneccion a la base de datos ya que siempre se cierra la conexion

--- EjemploIS2018-2/src/main/java/mx/unam/ciencias/is/modelo/EmpleadoDAO.java
+++ EjemploIS2018-2/src/main/java/mx/unam/ciencias/is/modelo/EmpleadoDAO.java
@@ -5,10 +5,57 @@
  */
 package mx.unam.ciencias.is.modelo;
 
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author jonathan
  */
 public class EmpleadoDAO {
+    private SessionFactory sessionFactory;
+    
+    
+    /**
+     * Constructor de la clase.
+     * inicializa la variable sessionFactory para que podamos abrir una session
+    */
+    public EmpleadoDAO(){
+        this.sessionFactory = HibernateUtil.getSessionFactory();
+    }
+     /**
+     * Regresa una lista con todos los empleados que estan activos en la base de datos.
+     * @return 
+     */
+    public List<Empleado> empleados() {
+        List<Empleado> result = null;
+        // arbrimos la sesion son sessionFactory 
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+            //iniciamos la transaccion, la consulta a realizar
+            tx = session.beginTransaction();
+            //Escribimos la consulta en HQL
+            String hql = "from Empleado";
+            Query query = session.createQuery(hql);
+            result = (List<Empleado>)query.list();
+            tx.commit();
+        }
+        catch (Exception e) {
+            //si hay un problema regresamos la base aun estado antes de la consulta
+            if (tx!=null){
+                tx.rollback();
+           }
+           e.printStackTrace(); 
+        }finally {
+            //cerramos la session
+            session.close();
+        }
+        return result;
+    }
+
     
 }
